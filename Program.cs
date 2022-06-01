502bcecd03f20bd3868439dc6b19158c3ee932a2
“AniketChivhe” <aniketchivhe@gmail.com>
2022-06-02 00:59:23 +0530

    [Aniket] UC1,UC2 for Creating and adding contacts are added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------Welcome To Adderess Book Program-------------");
+            AddressBookSystem.UC1UC2_CreateAndAddingContacts.GetContact();
+            AddressBookSystem.UC1UC2_CreateAndAddingContacts.ListContact();
+        }
+    }
+}
