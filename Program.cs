eb6e56b97d36b2c69b96c106719c3ad844f650e6
“AniketChivhe” <aniketchivhe@gmail.com>
2022-06-02 01:01:46 +0530

    [Aniket] UC3,UC4 for edit and delete contact are added

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace UC3UC4_EditAndDeleteContacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adding Contact
+            Console.WriteLine("------------Welcome To Adderess Book Program-------------");
+            Console.WriteLine("1.Enter to add the details\n2.Enter to modify the details");
+            Console.WriteLine("3.Listing the details..");
+            Console.WriteLine("4.Remove or Delete the  COntact details");
+            Console.WriteLine("Enter a option");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    EditAndDeleteContact.GetContact();  //adding 1st contact in Address book
+                    EditAndDeleteContact.ListContact();
+
+                    break;
+                case "2":
+                    EditAndDeleteContact.GetContact();
+                    EditAndDeleteContact.EditContact();//editing contact details
+                    EditAndDeleteContact.ListContact();
+                    break;
+                case "3":
+                    EditAndDeleteContact.GetContact();
+                    EditAndDeleteContact.ListContact();//listing the contact in address book
+                    break;
+                case "4":
+                    EditAndDeleteContact.GetContact();
+                    EditAndDeleteContact.DeletePeople();//Deleting Contact
+
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+
+            }
+
+
+        }
+    }
+}
