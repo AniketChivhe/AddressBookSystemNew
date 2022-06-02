50c003f07658722891e2f74cbd9b5d0ba2d3d7f4
“AniketChivhe” <aniketchivhe@gmail.com>
2022-06-03 01:55:02 +0530

    [Aniket]UC6 for adding contacts using dictionary added

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace UC6_DictionaryContacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adding Contact
+            Console.WriteLine("------------Welcome To Adderess Book Program-------------");
+            Console.WriteLine("1.Enter to add the details\n2.Enter to modify the details");
+            Console.WriteLine("3.Listing the details..");
+            Console.WriteLine("4.Remove or Delete the  COntact details");
+            Console.WriteLine("5.Multiplke Contact Details /Dictionary Added");
+            Console.WriteLine("Enter a option");
+            UC6AddressBook addressBook = new UC6AddressBook();
+            switch (Console.ReadLine())
+            {
+                case "1":
+
+                    addressBook.GetContact();
+                    addressBook.ListContact();
+                    break;
+                case "2":
+                    addressBook.GetContact();
+                    addressBook.EditContact();
+                    addressBook.ListContact();
+                    break;
+                case "3":
+                    addressBook.GetContact();
+                    addressBook.ListContact();
+                    break;
+                case "4":
+                    addressBook.GetContact();
+                    addressBook.DeletePeople();
+                    break;
+                case "5":
+                    addressBook.GetContact();
+                    addressBook.GetContact();
+                    addressBook.ListContact();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+
+            }
+        }
+    }
+}
